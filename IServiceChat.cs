92cb498d6352fbcde234064ffc003efb7b8fe6f5
Stepan Rayjkevich <74433490+Zemavong@users.noreply.github.com>
2022-04-02 12:27:01 +0300

    Beginning of work

--- IServiceChat.cs
+++ IServiceChat.cs
@@ -8,10 +8,24 @@ using System.Text;
 namespace wcf_chat
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceChat" in both code and config file together.
-    [ServiceContract]
+    [ServiceContract(CallbackContract = typeof(IServerChatCallBack))]
     public interface IServiceChat
     {
+
         [OperationContract]
-        void DoWork();
+        int Connect();
+
+        [OperationContract]
+        void Disconnect();
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string message);
+
+    }
+
+    public interface IServerChatCallBack
+    {
+        void MessageCallBack(string msg);
+
     }
 }
--- ServiceChat.cs
+++ ServiceChat.cs
@@ -10,8 +10,23 @@ namespace wcf_chat
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceChat" in both code and config file together.
     public class ServiceChat : IServiceChat
     {
+        public int Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
         public void DoWork()
         {
+        }
+
+        public void SendMessage(string message)
+        {
+            throw new NotImplementedException();
         }
     }
 }
